32c24fc2370da41ab8c955a423c6b3591da025ce
NathanAlbuquerque <nathan.nra@outlook.com>
2022-04-20 14:59:56 -0300

    Criado a view

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -3,17 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <title>Upload de imagens</title>
+    <style>
+        .container {
+            max-width: 500px;
+        }
+        dl, ol, ul {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+    </style>
 </head>
 <body>
-    
-    <form method="post" enctype="multipart/form-data" action="recebeUpload.php">
-        Selecione uma imagem: 
-        <input name="arquivo" type="file"/>
-        <br />
-        <input type="submit" value="Salvar"/>
-    </form>
-    
+    <div class="container mt-5">
+        <form action="{{route('fileUpload')}}" method="post" enctype="multipart/form-data">
+          <h3 class="text-center mb-5">Upload File in Laravel</h3>
+            @csrf
+            @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <strong>{{ $message }}</strong>
+            </div>
+          @endif
+          @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                      <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+          @endif
+            <div class="custom-file">
+                <input type="file" name="file" class="custom-file-input" id="chooseFile">
+                <label class="custom-file-label" for="chooseFile">Select file</label>
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary btn-block mt-4">
+                Upload Files
+            </button>
+        </form>
+    </div>
 </body>
 </html> 

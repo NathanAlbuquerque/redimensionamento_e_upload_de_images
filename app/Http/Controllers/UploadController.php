6739978431ab33e6f42610f4b0b017cebe84b4ff
NathanAlbuquerque <nathan.nra@outlook.com>
2022-04-20 14:56:26 -0300

    Criado o controlador

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -6,8 +6,25 @@ use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
-    public function index()
+    public function createForm()
     {
         return view('upload');
+    }
+    public function fileUpload(Request $req)
+    {
+        $req->validate([
+            'file' => 'required|mimes:csv,txt,xlx,xls,pdf|max:2048'
+        ]);
+        $fileModel = new File;
+        if($req->file()) {
+            $fileName = time().'_'.$req->file->getClientOriginalName();
+            $filePath = $req->file('file')->storeAs('uplaods', $fileName, 'public');
+            $fileModel->name = time().'_'.$req->file->getClientOriginalName();
+            $fileModel->file_path = '/storage/' . $filePath;
+            $fileModel->save();
+            return back()
+            ->with('sucesso','O arquivo foi carregado.')
+            ->with('file', $filename);
+        }
     }
 }
